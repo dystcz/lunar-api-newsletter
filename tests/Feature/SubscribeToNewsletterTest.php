04ee94067b819e9435d8dd1ea9af5b9f84d655c7
Jakub Theimer <jakub@dy.st>
2023-04-04 20:01:32 +0200

    add test

--- tests/Feature/SubscribeToNewsletterTest.php
+++ tests/Feature/SubscribeToNewsletterTest.php
@@ -8,7 +8,7 @@ use function Pest\Faker\fake;
 
 uses(TestCase::class, RefreshDatabase::class);
 
-test('user can subscribe to newsletter', function () {
+it('user can subscribe to newsletter', function () {
     $data = [
         'type' => 'newsletters',
         'attributes' => [
@@ -23,6 +23,29 @@ test('user can subscribe to newsletter', function () {
         ->post('/api/v1/newsletters/-actions/subscribe');
 
     $response->assertSuccessful();
+});
+
+it('requires emails in order to sign up', function () {
+    $data = [
+        'type' => 'newsletters',
+        'attributes' => [
+            'email' => '',
+        ],
+    ];
+
+    $response = $this
+        ->jsonApi()
+        ->expects('newsletters')
+        ->withData($data)
+        ->post('/api/v1/newsletters/-actions/subscribe');
+
+    $expected = [
+        'source' => ['pointer' => '/data/attributes/email'],
+        'status' => '422',
+        'detail' => 'Please enter an email address',
+    ];
+
+    $response->assertError(422, $expected);
 });
 
 it('doesnt accept invalid emails', function () {
