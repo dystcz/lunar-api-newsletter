fe74de9dd569bcb197ad9e3d84fef46880888e16
Jakub Theimer <jakub@dy.st>
2023-04-04 19:55:59 +0200

    add config

--- config/lunar-newsletter.php
+++ config/lunar-newsletter.php
@@ -1,8 +1,24 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
 
+    // Prefix for all the API routes
+    // Leave empty if you don't want to use a prefix
+    'route_prefix' => 'api',
+
+    // Middleware for all the API routes
+    'route_middleware' => ['api'],
+
+    // Configuration for specific domains
+    'domains' => [
+        'newsletters' => [
+
+            // Route groups which get registered
+            // If you want to change the behaviour or add some data,
+            // simply extend the package product groups and add your logic
+            'route_groups' => [
+                'newsletters' => Dystcz\LunarNewsletter\Domain\Newsletter\Http\Routing\NewsletterRouteGroup::class,
+            ],
+        ],
+    ],
 ];
