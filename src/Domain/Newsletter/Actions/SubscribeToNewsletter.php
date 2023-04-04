2d86e79f0313f53e281b3342133397f73a9b200d
Jakub Theimer <jakub@dy.st>
2023-04-04 20:40:13 +0200

    subscribe to newsletter

--- src/Domain/Newsletter/Actions/SubscribeToNewsletter.php
+++ src/Domain/Newsletter/Actions/SubscribeToNewsletter.php
@@ -2,9 +2,15 @@
 
 namespace Dystcz\LunarNewsletter\Domain\Newsletter\Actions;
 
+use Spatie\Newsletter\Facades\Newsletter;
+
 class SubscribeToNewsletter
 {
+    /**
+     * Subscribe to newsletter.
+     */
     public function __invoke(string $email): void
     {
+        Newsletter::subscribe($email);
     }
 }
