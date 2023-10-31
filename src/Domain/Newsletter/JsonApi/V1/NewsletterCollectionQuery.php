81f56a860bddd22466c775ec541bdee3e6003a2c
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 19:09:40 +0100

    fix namespace

--- src/Domain/Newsletter/JsonApi/V1/NewsletterCollectionQuery.php
+++ src/Domain/Newsletter/JsonApi/V1/NewsletterCollectionQuery.php
@@ -2,7 +2,7 @@
 
 namespace Dystcz\LunarApiNewsletter\Domain\Newsletter\JsonApi\V1;
 
-use Dystcz\LunarApi\Domain\Collections\JsonApi\V1\CollectionQuery;
+use Dystcz\LunarApi\Domain\JsonApi\Queries\CollectionQuery;
 
 class NewsletterCollectionQuery extends CollectionQuery
 {
