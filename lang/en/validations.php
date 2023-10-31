4754e16ea2fbb7e94e34adff8989f222d7bf49e0
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2023-10-31 09:23:52 +0100

    add localized validation messages

---
+++ lang/en/validations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'newsletter_subscription' => [
+        'email' => [
+            'required' => 'Please enter an email address',
+            'email' => 'Please enter a valid email address',
+        ],
+    ],
+];
