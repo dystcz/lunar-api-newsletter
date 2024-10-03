d28c71bc4141c612a731f1f482592c46dba70f4e
Jakub Theimer <5587309+theimerj@users.noreply.github.com>
2024-10-03 13:38:31 +0200

    add czech validation messages

---
+++ lang/cs/validations.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'newsletter_subscription' => [
+        'email' => [
+            'required' => 'Prosím zadejte emailovou adresu',
+            'email' => 'Zadejte prosím platnou emailovou adresu',
+        ],
+    ],
+];
